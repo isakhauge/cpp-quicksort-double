f1d7e3b15b00c350dc869983a33bdd5a1c644933
Isak Hauge <isakhauge@gmail.com>
2019-09-22 23:54:29 -0700

    Create documentation and conduct refactoring.

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@ void print(double arr[], int length);
 int main() {
 	#define N 10
 	double test2[N] = {6.0,4.0,1.0,10.1,20.0,5.5,31.2,9.9,0,3.3};
-	//double test2[N] = {6,4,1,10,20,5,31,9,0,3};
-	//double test2[N] = {9,4};
 	print(test2,N);
 	ikh::quickSort(test2, 0, N, N);
 	print(test2,N);
--- quicksort.h
+++ quicksort.h
@@ -24,38 +24,66 @@ namespace ikh {
 	/**
 	 * Partition
 	 * @param arr An array of double values.
-	 * @param left
-	 * @param right
-	 * @param length
-	 * @return
+	 * @param left A defined start index.
+	 * @param right A defined end index.
+	 * @param length The total length of the array.
+	 * @return An index representing the partition split point.
 	 */
 	static int partition(double arr[], int left, int right, int length) {
-		double* pivot = &arr[left];
-		int l = left;
-		int r = right;
 
-		while (l < r) {
-			do {l++;} while (arr[l] <= *pivot and l < length);
-			do {r--;} while (arr[r] > *pivot and r >= 0);
-			if (l < r)
-				if (arr[l] > *pivot and arr[r] < *pivot)
-					swap(&arr[l], &arr[r]);
+		// Create pivot pointer for semantics.
+		double* pivot = &arr[left];
+
+		// Copy indexes for manipulation.
+		int leftIndex = left;
+		int rightIndex = right;
+
+		while (leftIndex < rightIndex) {
+			// Find left value larger than pivot.
+			do {
+				leftIndex++;
+			} while (arr[leftIndex] <= *pivot and leftIndex < length);
+
+			// Find right value smaller than pivot.
+			do {
+				rightIndex--;
+			} while (arr[rightIndex] > *pivot and rightIndex >= 0);
+
+			// ? If values are eligible for swapping.
+			if (leftIndex < rightIndex and arr[leftIndex] > *pivot and arr[rightIndex] < *pivot)
+				swap(&arr[leftIndex], &arr[rightIndex]);
 		};
-		cout << "L: " << l << endl;
-		cout << "R: " << r << endl;
-		swap(&*pivot, &arr[r]);
-		return l;
+
+		/* ! NOTE: At this point, the indexes have crossed each other. */
+		// Swap the pivot with the value at the current right index.
+		swap(&*pivot, &arr[rightIndex]);
+
+		// Return the new start index.
+		return leftIndex;
 	}
 
+
+	/**
+	 * Quick Sort
+	 * @param arr An array of double values.
+	 * @param left A defined start index.
+	 * @param right A defined end index.
+	 * @param length The total length of the array.
+	 */
 	static void quickSort(double arr[], int left, int right, int length) {
-		if (left >= right) return;
-		int newLeft = partition(arr, left, right, length);
+
+		if (left >= right)
+			return; // Cancel method.
+
+		// Partition array, and return a new partition start index.
+		int startIndex = partition(arr, left, right, length);
+
 		if (left < right) {
-			quickSort(arr, left, newLeft - 1, length);
-			quickSort(arr, newLeft, right, length);
+			// Quick sort the left partition of the array.
+			quickSort(arr, left, startIndex - 1, length);
+			// Quick sort the right partition of the array.
+			quickSort(arr, startIndex, right, length);
 		}
 	}
 
 }
-
-#endif //QUICKSORT_QUICKSORT_H
