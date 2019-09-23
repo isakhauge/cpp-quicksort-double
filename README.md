daa3b698ab0b7e9677d8e61b7bcb6e62a91a729e
Isak Hauge <isakhauge@gmail.com>
2019-09-23 00:28:03 -0700

    Refactor demo and improve readme file.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# cpp-quicksort-double
+# Quick sort - C++
 A quick sort algorithm in C++ for double values.
+
+## Usage
+The main code is inside header file "quicksort.h".
+
+## Methods
+### Swap
+This method swaps two values using pointers.
+
+### Partition
+This method partitions the array and returns a new start index for the right partition.
+
+### Quick sort
+This method calls itself recursively.
---
+++ quickSortDemo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include "quicksort.h"
+
+#define N 10
+static double demo[N] = {6.0,4.0,1.0,10.1,20.0,5.5,31.2,9.9,0,3.3};
+
+using namespace std;
+
+void print(double arr[], int length) {
+	std::string separator = "";
+	std::cout << "[";
+	for (int i=0; i<length; i++)
+		std::cout << ((i==0) ? "" : ", ") << arr[i];
+	std::cout << "]\n";
+}
+
+void startDemo() {
+	cout << "Before sort: ";
+	print(demo,N);
+	ikh::quickSort(demo, 0, N, N);
+	cout << "After sort: ";
+	print(demo,N);
+}
+
+int main() {
+	startDemo();
+	return 0;
+}
